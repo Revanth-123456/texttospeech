a83e3e79be20bcd4ca932e444094d0371e914288
Revanth V S <revanthvs04@gmail.com>
2025-06-12 12:55:27 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.png';
+
+const Header = () => {
+
+    return (
+        <>
+            <img src={logo} alt="logo" style={{height: 42}} />
+        </>
+    )
+}
+
+export default Header;
